a827bbac99a148955c6880c02a88fe39848367b5
Emil-87 <emil.pegas@gmail.com>
2022-10-02 17:08:00 +0600

    ex010 MethodArray

---
+++ ex010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {13,24,35,47,58,35,14,26,18,};
+
+int n = array.Length;
+int find = 35;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find) 
+{
+        Console.WriteLine(index);
+        break;
+}
+    //index = index +1;
+    index++;
+}
